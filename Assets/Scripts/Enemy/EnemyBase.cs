fd4612270afff50f8f72538f847c5d5f0402d00b
Hiromasa <cehi24150@g.nihon-u.ac.jp>
2025-09-26 23:13:48 +0900

    テスト開発01

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 /// <summary>
 /// 全エネミー共通処理クラス
@@ -14,14 +15,28 @@ public class EnemyBase : MonoBehaviour
     protected SpriteRenderer spriteRenderer; //　敵スプライト
     protected Transform actorTransform; //主人公(アクター)のTransform
 
+    //画像素材
+    public Sprite sprite_Defeat; // 被撃破スプライト
+
     //各種変数
     //基礎データ(インスペクタから入力)
     [Header("最大体力(初期体力)")] public int maxHP;
     [Header("接触時アクターへのダメージ")] public int touchDamage;
+    [Header("ボス敵フラグ(ONでボス敵として扱う。1ステージに1体のみ)")] public bool isBoss;
     //その他データ
     [HideInInspector] public int nowHP; //残りHP
+    [HideInInspector] public bool isVanishing; //消滅フラグ trueで消滅中
     [HideInInspector] public bool isInvis; //無敵モード
     [HideInInspector] public bool rightFacing; //右向きフラグ(falseで左向き)
+
+    //DoTween用
+    private Tween damageTween; //被ダメージ時演出Tween
+
+    //定数定義
+    private readonly Color COL_DEFAULT = new Color(1.0f, 1.0f, 1.0f, 1.0f); //通常
+    private readonly Color COL_DAMAGED = new Color(1.0f, 0.1f, 0.1f, 1.0f); //被ダメージ時カラー
+    private const float KNOCKBACK_X = 1.8f; //被ダメージ時ノックバック力(x方向)
+    private const float KNOCKBACK_Y = 0.3f; //被ダメージ時ノックバック力(y方向)
 
     // 初期化関数(AreaManager.csから呼出)
     public void Init(AreaManager _areaManager)
@@ -32,7 +47,8 @@ public class EnemyBase : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
 
-        //変数初期化    
+        //変数初期化 
+        rigidbody2D.freezeRotation = true;   
         nowHP = maxHP;
         if (actorTransform.localScale.x > 0.0f)
             rightFacing = true;
@@ -63,10 +79,49 @@ public class EnemyBase : MonoBehaviour
 
         if (nowHP <= 0.0f)
         { //HP0の場合
-            Vanish();
+          //被ダメージTween初期化
+            if (damageTween != null)
+                damageTween.Kill();
+            damageTween = null;
+
+            //消滅中フラグをセット
+            isVanishing = true;
+            //消滅中は物理演算なし
+            rigidbody2D.velocity = Vector2.zero;
+            rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
+            //点滅後に消滅処理を呼び出す(DoTween使用)
+            spriteRenderer.DOFade(0.0f, 0.15f) // 0.15秒*ループ回数分の再生時間
+                .SetEase(Ease.Linear) //線形変化
+                .SetLoops(7, LoopType.Yoyo) //7回点滅
+                .OnComplete(Vanish); //再生が終わったらVanish()を呼び出す設定
+
+            // 被撃破時スプライトがあれば表示
+            if (sprite_Defeat != null)
+                spriteRenderer.sprite = sprite_Defeat;
+
+            // その他撃破時処理
+            if (isBoss)
+            { //ボス撃破
+
+            }
+            else
+            { //ザコ撃破
+
+            }
         }
         else
         { //まだHPが残っている場合
+            // 被ダメージTween初期化
+            if (damageTween != null)
+                damageTween.Kill();
+            damageTween = null;
+            //被ダメージ演出再生
+            // (一瞬だけスプライトを赤色に変更)
+            if (!isInvis)
+            {
+                spriteRenderer.color = COL_DAMAGED; //赤色に変更
+                damageTween = spriteRenderer.DOColor(COL_DEFAULT, 1.0f); //DoTweenで徐々に戻す
+            }
 
         }
 
@@ -87,8 +142,11 @@ public class EnemyBase : MonoBehaviour
     /// </summary>
     public void BodyAttack(GameObject actorObj)
     {
+        //自身が消滅中なら無効
+        if (isVanishing)
+            return;
         //アクターのコンポーネントを取得
-        ActorController actorCtrl = actorObj.GetComponent<ActorController>();
+            ActorController actorCtrl = actorObj.GetComponent<ActorController>();
         if (actorCtrl == null)
             return;
 
--- Assets/Scripts/Enemy/Enemy_Snake.cs
+++ Assets/Scripts/Enemy/Enemy_Snake.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 /// <summary>
@@ -10,10 +11,18 @@ using UnityEngine;
 /// </summary>
 public class Enemy_Snake : EnemyBase
 {
-    // Start
-    void Start()
-    {
-    }
+    //設定項目
+    [Header("移動速度")]
+    public float movingSpeed;
+    [Header("最大移動速度")]
+    public float maxSpeed;
+    [Header("移動条件(アクターとの距離がこの値以下なら移動)")]
+    public float awakeDistance;
+    [Header("非移動時減速率")]
+    public float brakeRatio;
+
+    //各種変数
+    private bool isBreaking; //ブレーキ作動フラグ trueで減速
 
     /// <summary>
     /// このモンスターの居るエリアにアクターが侵入した時の起動時処理(エリアアクティブ化時処理)
@@ -22,12 +31,62 @@ public class Enemy_Snake : EnemyBase
     {
         //元々の起動時処理を実行
         base.OnAreaActivated();
-
-        Debug.Log("追加の起動時処理");
     }
 
     // Update
     void Update()
     {
+        //消滅中なら処理しない
+        if (isVanishing)
+            return;
+
+        // アクターが近くにいると接近する処理
+        float speed = 0.0f; // x方向移動速度
+        Vector2 ePos = transform.position; // エネミー座標
+        Vector2 aPos = actorTransform.position; //　アクター座標
+
+        // アクターとの距離が離れている場合はブレーキフラグを立て終了(移動しない)
+        if (Vector2.Distance(ePos, aPos) > awakeDistance) //Vector2.Distance(a, b)で aとbの距離を参照
+        {
+            isBreaking = true;
+            return;
+        }
+        isBreaking = false; //離れてないならブレーキフラグfalse
+
+        //アクターとの位置関係から向きを決定
+        if (ePos.x > aPos.x)
+        { //左向き
+            speed = -movingSpeed;
+            SetFacingRight(false);
+        }
+        else
+        { //右向き
+            speed = movingSpeed;
+            SetFacingRight(true);
+        }
+
+
+        //移動処理
+        Vector2 vec = rigidbody2D.velocity; //速度ベクトル
+        vec.x += speed * Time.deltaTime;
+        //x方向の速度の最大値を設定
+        if (vec.x > 0.0f) //右方向
+            vec.x = Mathf.Clamp(vec.x, 0.0f, maxSpeed);
+        else //左方向
+            vec.x = Mathf.Clamp(vec.x, -maxSpeed, 0.0f);
+        //速度ベクトルをセット
+        rigidbody2D.velocity = vec;
+    }
+
+    //FixedUpdate
+    void FixedUpdate()
+    {
+        //アクターが近くに居ない時のブレーキ処理
+        if (isBreaking)
+        {
+            Vector2 vec = rigidbody2D.velocity; //エネミー速度
+            vec.x *= brakeRatio; // x方向のみ減速
+            rigidbody2D.velocity = vec;
+        }
     }
 }
